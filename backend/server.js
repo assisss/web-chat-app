fadc2666a0eea11c45120265920b4b9eebb5e884
assisss <111203064+assisss@users.noreply.github.com>
2024-08-04 01:34:41 +0530

    ajdusting frontend url

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,7 @@ const server = http.createServer(app);
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000", // Adjust to your frontend URL
+    origin: "https://web-chat-app-7-dq9h.onrender.com/", // Adjust to your frontend URL
     credentials: true,
   },
 });
